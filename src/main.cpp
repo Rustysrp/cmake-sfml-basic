6e498ecf6baff471d4e8fe5e5afc84b9fdd0a643
Rustysrp <spunkeyy0@gmail.com>
2023-10-25 11:21:00 -0400

    final test

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,19 @@ int main()
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
 
-    // auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project", sf::Style::Default, settings };
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project"};
+    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project", sf::Style::Fullscreen, settings };
     window.setFramerateLimit(60);
 
-    sf::CircleShape circle(50.f);
-    circle.setPosition(0, 0);
+    auto radius = 50.f;
+    sf::CircleShape circle(radius);
+    circle.setPosition((window.getSize().x / 2) - radius, 10.f);
 
     float mv_velo = 0;
 
     while (window.isOpen())
     {
+        auto mid_s = (window.getSize().x / 2) - radius;
+
         for (auto event = sf::Event{}; window.pollEvent(event);)
         {
             if (event.type == sf::Event::Closed) {
@@ -24,13 +26,13 @@ int main()
             }
 
             if (event.type == sf::Event::KeyPressed) {
-                circle.setPosition(0.f, 0.f);
+                circle.setPosition(mid_s, 10.f);
             }
         }
 
 
         // Clear the window w/ a black bg
-        window.clear();
+        window.clear(sf::Color::Black);
 
         // Move circle
         circle.move(0, mv_velo);
@@ -39,13 +41,14 @@ int main()
         mv_velo += 0.95;
 
         // Check bounds
-        if (circle.getPosition().y >= (window.getSize().y - 100.f)) {
+        if (circle.getPosition().y >= (window.getSize().y - radius * 2)) {
             // Reverse & dampen
             mv_velo = mv_velo * -0.9;
+            circle.setPosition(mid_s, window.getSize().y - radius * 2);
         }
 
         // Set color
-        circle.setFillColor(sf::Color(100, 250, 50));
+        circle.setFillColor(sf::Color(100, 250 * (1 / mv_velo), 100 * (1 / mv_velo)));
 
         // Draw entities
         window.draw(circle);
